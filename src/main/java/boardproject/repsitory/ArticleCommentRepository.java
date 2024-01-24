5a0e53b72572ce02dcbe0bcf226b06cc36273b1b
Yoon321 <tkdwls734@gmail.com>
2024-01-25 08:01:20 +0900

    #1 - Querydsl 지원을 이용하여 api 검색 옵션 추가
    'QuerydslpredeicateExcutor'
    'QuerydslBinderCustomizer' 를 이용하여 간단하게 개발

--- src/main/java/boardproject/repsitory/ArticleCommentRepository.java
+++ src/main/java/boardproject/repsitory/ArticleCommentRepository.java
@@ -1,9 +1,34 @@
 package boardproject.repsitory;
 
 import boardproject.domain.ArticleComment;
+import boardproject.domain.QArticle;
+import boardproject.domain.QArticleComment;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+public interface ArticleCommentRepository extends
+        JpaRepository<ArticleComment, Long>,
+        QuerydslPredicateExecutor<ArticleComment>,
+        QuerydslBinderCustomizer<QArticleComment>
+{
+    @Override
+    default void customize(QuerydslBindings bindings, QArticleComment root) {
+        // 현재는 'QuerydslPredicateExecutor<Article>' 문구로 인해서 모든 필드가 검색 필드로 들어가는데 이것은 우리가 원한 것이 아니다.
+        // 기본값이 false 인데 true 로 변경해줘서 모든 필드가 검색 필드로 가는 것을 막음
+        bindings.excludeUnlistedProperties(true);
+
+        // 검색 필드에 추가함
+        bindings.including(root.content, root.createdAt, root.createdBy);
+
+        // 기존에 정확히 검색해야 하는 부분을 변경
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
--- src/main/java/boardproject/repsitory/ArticleRepository.java
+++ src/main/java/boardproject/repsitory/ArticleRepository.java
@@ -1,9 +1,37 @@
 package boardproject.repsitory;
 
 import boardproject.domain.Article;
+import boardproject.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.time.DateTimeException;
+import java.util.zip.DataFormatException;
+
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>,
+        QuerydslBinderCustomizer<QArticle> {
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) {
+        // 현재는 'QuerydslPredicateExecutor<Article>' 문구로 인해서 모든 필드가 검색 필드로 들어가는데 이것은 우리가 원한 것이 아니다.
+        // 기본값이 false 인데 true 로 변경해줘서 모든 필드가 검색 필드로 가는 것을 막음
+        bindings.excludeUnlistedProperties(true);
+
+        // 검색 필드에 추가함
+        bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy);
+
+        // 기존에 정확히 검색해야 하는 부분을 변경
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
