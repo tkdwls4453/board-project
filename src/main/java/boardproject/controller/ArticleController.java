13fedf2badeeeabf65da23be643497de006b56ef
Yoon321 <tkdwls734@gmail.com>
2024-02-02 22:18:31 +0900

    #26 - 페이지네이션 기능을 컨트롤러(게시판 뷰)에 반영
    테스트는 'Page' 클래스를 이용하기 때문에
    하는 김에 페이징과 정렬을 함께 테스트
     나중에 정렬 구현에서 이어서 활용할 전망

--- src/main/java/boardproject/controller/ArticleController.java
+++ src/main/java/boardproject/controller/ArticleController.java
@@ -4,7 +4,9 @@ import boardproject.domain.type.SearchType;
 import boardproject.dto.response.ArticleResponse;
 import boardproject.dto.response.ArticleWithCommentsResponse;
 import boardproject.service.ArticleService;
+import boardproject.service.PaginationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -15,6 +17,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 
 @RequiredArgsConstructor
 @RequestMapping("/articles")
@@ -22,6 +26,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class ArticleController {
 
     private final ArticleService articleService;
+    private final PaginationService paginationService;
     @GetMapping
     public String articles(
             @RequestParam(name = "searchType", required = false) SearchType searchType,
@@ -29,7 +34,12 @@ public class ArticleController {
             @PageableDefault(size = 10, sort="createdAt", direction = Sort.Direction.DESC) Pageable pageable,
             ModelMap map
     ) {
-        map.addAttribute("articles", articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from));
+        Page<ArticleResponse> articles = articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from);
+        List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
+
+        map.addAttribute("articles", articles);
+        map.addAttribute("paginationBarNumbers", barNumbers);
+
         return "articles/index";
     }
 
