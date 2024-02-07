ca0cb631c3befe205977ced2471bf5769313080f
Yoon321 <tkdwls734@gmail.com>
2024-02-07 20:45:10 +0900

    #33 - 해시태그 검색 페이지를 위한 기능 구현
    해시태그 뷰에 모든 게시글에서 나오는 해시태그를 정리해서 보여주는 기능을 넣기 위해,
    해시태그만 유니크하게 보여주는 쿼리를 querydsl 로 작성
    그 외 서비스 코드, 컨트롤러 관련 테스트 작성

--- src/main/java/boardproject/controller/ArticleController.java
+++ src/main/java/boardproject/controller/ArticleController.java
@@ -27,11 +27,12 @@ public class ArticleController {
 
     private final ArticleService articleService;
     private final PaginationService paginationService;
+
     @GetMapping
     public String articles(
             @RequestParam(name = "searchType", required = false) SearchType searchType,
             @RequestParam(name = "searchValue", required = false) String searchValue,
-            @PageableDefault(size = 10, sort="createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
             ModelMap map
     ) {
         Page<ArticleResponse> articles = articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from);
@@ -56,4 +57,21 @@ public class ArticleController {
         return "articles/detail";
     }
 
+    @GetMapping("/search-hashtag")
+    public String searchHashtag(
+            @RequestParam(name = "searchValue", required = false) String searchValue,
+            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+            ModelMap map
+    ){
+        Page<ArticleResponse> articles = articleService.searchArticlesViaHashtag(searchValue, pageable).map(ArticleResponse::from);
+        List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
+        List<String> hashtags = articleService.getHashtags();
+
+        map.addAttribute("articles", articles);
+        map.addAttribute("hashtags", hashtags);
+        map.addAttribute("paginationBarNumbers", barNumbers);
+        map.addAttribute("searchType", SearchType.HASHTAG);
+
+        return "articles/search-hashtag";
+    }
 }
